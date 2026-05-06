dab6f997a4f11678f6a67752487c09053d66698c
kjp0411 <98109773+kjp0411@users.noreply.github.com>
2026-05-06 15:08:16 +0900

    feat: add Keycloak admin client configuration

---
+++ src/main/java/org/ticketing/user/infrastructure/config/KeycloakConfig.java
@@ -0,0 +1,30 @@
+package org.ticketing.user.infrastructure.config;
+
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfig {
+
+    @Bean
+    public Keycloak keycloakAdminClient(
+        @Value("${keycloak.server-url}") String serverUrl,
+        @Value("${keycloak.admin-realm}") String adminRealm,
+        @Value("${keycloak.admin-client-id}") String adminClientId,
+        @Value("${keycloak.admin-username}") String adminUsername,
+        @Value("${keycloak.admin-password}") String adminPassword
+    ) {
+        return KeycloakBuilder.builder()
+            .serverUrl(serverUrl)
+            .realm(adminRealm)
+            .clientId(adminClientId)
+            .username(adminUsername)
+            .password(adminPassword)
+            .grantType(OAuth2Constants.PASSWORD)
+            .build();
+    }
+}
